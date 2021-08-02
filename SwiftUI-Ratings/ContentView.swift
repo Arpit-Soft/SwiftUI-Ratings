00e412abf3cf622860480951e5a888b7eb36943f
Arpit Dixit <arpitd@dewsolutions.in>
2021-08-02 15:29:50 +0530

    Initial Commit

---
+++ SwiftUI-Ratings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Ratings
+//
+//  Created by Arpit Dixit on 02/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
